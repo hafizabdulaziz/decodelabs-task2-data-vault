7e2cf04e17d159e3858c0e0f4658acabd7ee5d3e
Abdul Aziz <abdul.aziz@example.com>
2026-09-15 00:05:54 +0500

    feat: configure PostgreSQL migration

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,8 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from app.core.config import settings
+from app.models.user import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,17 +20,15 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 
 def run_migrations_offline() -> None:
+
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
